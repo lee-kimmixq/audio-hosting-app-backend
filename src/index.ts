cea2bb7a6693ea8e4ab3c703080b34d5ced890b5
Kimmi Lee <lee.kimmixq@gmail.com>
2023-04-23 00:11:08 +0800

    feat: setup eslint, prettier, express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello");
+import express from 'express'
+import cors from 'cors'
+import './config'
+
+const { FRONTEND_URL } = process.env
+
+const app = express()
+
+app.use(
+  cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+  })
+)
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/test', (_, res) => {
+  res.send('TEST works')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
